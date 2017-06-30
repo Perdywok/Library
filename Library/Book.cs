c5bf4a38b635ce56205fae5e4f5628249e47c450
Perdywok <kosras.96@gmail.com>
2017-06-30 20:51:09 +0300

    Add project files.

---
+++ Library/Book.cs
@@ -0,0 +1,140 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Library
+{
+    class Book : INotifyPropertyChanged
+    {
+        private string name;
+        private string autor;
+        private string cover;
+        private string genge;
+        private string publisher;
+        private string language;
+        private int price;
+        private int pages;
+        private int year;
+
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+                OnPropertyChanged("Name");
+            }
+        }
+        public string Autor
+        {
+            get
+            {
+                return autor;
+            }
+            set
+            {
+                autor = value;
+                OnPropertyChanged("Autor");
+            }
+        }
+
+        public string Cover
+        {
+            get
+            {
+                return cover;
+            }
+            set
+            {
+                cover = value;
+                OnPropertyChanged("Cover");
+            }
+        }
+
+        public string Genge
+        {
+            get
+            {
+                return genge;
+            }
+            set
+            {
+                genge = value;
+                OnPropertyChanged("Genge");
+            }
+        }
+
+        public string Publisher
+        {
+            get
+            {
+                return publisher;
+            }
+            set
+            {
+                publisher = value;
+                OnPropertyChanged("Publisher");
+            }
+        }
+        public string Language
+        {
+            get
+            {
+                return language;
+            }
+            set
+            {
+                language = value;
+                OnPropertyChanged("Language");
+            }
+        }
+        public int Year
+        {
+            get
+            {
+                return year;
+            }
+            set
+            {
+                year = value;
+                OnPropertyChanged("Year");
+            }
+        }
+        public int Pages
+        {
+            get
+            {
+                return pages;
+            }
+            set
+            {
+                pages = value;
+                OnPropertyChanged("Pages");
+            }
+        }
+        public int Price
+        {
+            get
+            {
+                return price;
+            }
+            set
+            {
+                price = value;
+                OnPropertyChanged("Price");
+            }
+        }
+
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
---
+++ Library/LibraryOfBooks.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Data;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Collections.ObjectModel;
+
+namespace Library
+{
+    class LibraryOfBooks : INotifyPropertyChanged
+    {
+        SqlConnection connection = new SqlConnection();
+        private Book selectedBook;
+        public ObservableCollection<Book> Books { get; set; }
+
+        // команда добавления нового объекта
+        private RelayCommand addCommand;
+        public RelayCommand AddCommand
+        {
+            get
+            {
+                return addCommand ??
+                  (addCommand = new RelayCommand(obj =>
+                  {
+                      Book book = new Book();
+                      Books.Insert(0, book);
+                      selectedBook = book;
+                  }));
+            }
+        }
+
+        // команда удаления
+        private RelayCommand removeCommand;
+        public RelayCommand RemoveCommand
+        {
+            get
+            {
+                return removeCommand ??
+                  (removeCommand = new RelayCommand(obj =>
+                  {
+                      Book book = obj as Book;
+                      if (book != null)
+                      {
+                          Books.Remove(book);
+                      }
+                  },
+                 (obj) => Books.Count > 0));
+            }
+        }
+
+        public Book SelectedBook
+        {
+            get { return selectedBook; }
+            set
+            {
+                selectedBook = value;
+                OnPropertyChanged("SelectedBook");
+            }
+        }
+
+        public LibraryOfBooks()
+        {
+            Books = new ObservableCollection<Book>
+            {
+
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
